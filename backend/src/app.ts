7c71fbda497b58e66a72fdccfd1ec29237d540f0
Marina Kamel <126111065+MarinaKamel-coder@users.noreply.github.com>
2026-02-18 09:49:29 -0500

    fix: supprime

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,8 +28,6 @@ if (!fs.existsSync(uploadDir)){
 app.use(cors({
   origin: ["https://cads-murex.vercel.app", "http://localhost:5173"],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 
