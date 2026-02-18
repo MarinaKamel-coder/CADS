04e8c77fe3f33ab473ba90c1267e4ab7c7d20f09
Marina Kamel <126111065+MarinaKamel-coder@users.noreply.github.com>
2026-02-18 09:17:38 -0500

    fix: efface un / dans le lien de vercel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,8 +26,10 @@ if (!fs.existsSync(uploadDir)){
 
 // 1. Middlewares de base
 app.use(cors({
-  origin: ["https://cads-murex.vercel.app/", "http://localhost:5173"],
-  credentials: true
+  origin: ["https://cads-murex.vercel.app", "http://localhost:5173"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 
